290935dac30387a1aa7fad5bf030f429c4274879
Hiroaki0117 <nishimura.hn.0117@gmail.com>
2024-04-15 17:48:38 +0900

    Header component Create

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import "./Header.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="Header">
+        <div className="headerRight">
+            <h3>#logo </h3>
+            <div className="headerSearch">
+                <SearchIcon />
+                <input type="text" placeholder='気になる選手を検索' />
+            </div>
+        </div>
+    </div>
   )
 }
 
